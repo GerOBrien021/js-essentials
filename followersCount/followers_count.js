f039b23ee76e0afa9869b3dbff83d6e1352ed2e5
GerOBrien021 <gerard.obrien.cork@gmail.com>
2025-10-09 11:41:51 -0400

    add reset function

--- followersCount/followers_count.js
+++ followersCount/followers_count.js
@@ -3,16 +3,27 @@ let count = 0;
 function increaseCount() {
     count++; // Increment the count by 1
     displayCount(); 
-    checkCountValue()
+    checkCountValue();
 }
 
+
+function resetCount() {
+    count = 0;
+    displayCount(); 
+    checkCountValue();
+}
+
+
 function checkCountValue() {
-    if (count === 10) {
-      alert("Your Instagram post gained 10 followers! Congratulations!");
+    if (count === 0) {
+      alert("The followers count has been reset to zero.");
+    } else if (count === 10) {
+      alert("Your Instagram post gained 10 followers! Keep it up!");
     } else if (count === 20) {
       alert("Your Instagram post gained 20 followers! Keep it up!");
     }
 }
+
 
 function displayCount() {
     document.getElementById('countDisplay').innerHTML = count; // Display the count in the HTML
