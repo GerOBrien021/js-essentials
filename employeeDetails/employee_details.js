d08f7e392eb8b9a224030ea6fcf09be20f061ad4
GerOBrien021 <gerard.obrien.cork@gmail.com>
2025-10-15 09:22:39 -0400

    add specialization filter

--- employeeDetails/employee_details.js
+++ employeeDetails/employee_details.js
@@ -1,7 +1,7 @@
 const employees = [
-    { id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000 },
-    { id: 2, name: 'Alice Smith', age: 28, department: 'HR', salary: 45000 },
-    { id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000 },
+    { id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000, specialization: 'JavaScript' },
+    { id: 2, name: 'Alice Smith', age: 28, department: 'HR', salary: 45000, specialization: 'JavaScript' },
+    { id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000, specialization: 'PHP' },
     //... More employee records can be added here
 ];
 
@@ -34,4 +34,9 @@ function findEmployeeById(employeeId) {
     }
  }
 
- 
+ function displayJSDevs() {
+    const jsDevs = employees.filter(employee => employee.specialization === 'JavaScript');
+    const jsDevsDisplay = jsDevs.map((employee, index) => `<p>${employee.id}: ${employee.name}: ${employee.name} - ${employee.department} - $${employee.salary} - ${employee.specialization}</p>`).join('');
+    document.getElementById('employeesDetails').innerHTML = jsDevsDisplay;
+}
+
